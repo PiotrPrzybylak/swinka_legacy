3a6c60e90da60c627a408d757eb10e2feebdb142
Piotr Przybylak <piotr.przybylak@rndity.com>
2017-05-28 17:30:57 +0200

    save sql structure

--- src/main/database/create_tables.sql
+++ src/main/database/create_tables.sql
@@ -1,20 +1,34 @@
-CREATE SEQUENCE public.events_id_seq
+-- Sequence: public.piggybanks_id_seq
+
+-- DROP SEQUENCE public.piggybanks_id_seq;
+
+CREATE SEQUENCE public.piggybanks_id_seq
   INCREMENT 1
   MINVALUE 1
   MAXVALUE 9223372036854775807
-  START 6
+  START 2
   CACHE 1;
+ALTER TABLE public.piggybanks_id_seq
+  OWNER TO ldyginzhuqjqyp;
 
-CREATE TABLE public.events
+
+-- Table: public.piggybanks
+
+-- DROP TABLE public.piggybanks;
+
+CREATE TABLE public.piggybanks
 (
-  eventname character varying,
-  city character varying,
-  dance character varying NOT NULL,
-  id integer NOT NULL DEFAULT nextval('events_id_seq'::regclass),
-  price double precision,
-  date date,
-  CONSTRAINT id PRIMARY KEY (id)
+  id integer NOT NULL DEFAULT nextval('piggybanks_id_seq'::regclass),
+  name text,
+  short_description text,
+  target numeric,
+  current numeric,
+  long_description text,
+  picture_url text,
+  CONSTRAINT piggybank_pk PRIMARY KEY (id)
 )
 WITH (
   OIDS=FALSE
 );
+ALTER TABLE public.piggybanks
+  OWNER TO ldyginzhuqjqyp;
