1820297b5b032393580d34e3e4ecea6300c1354c
Maksymilian Tomczyk <tomczyk.maksymilian@outlook.com>
2017-05-30 23:08:49 +0200

    Gulp works correctly

--- src/main/resources/templates/Gulpfile.js
+++ src/main/resources/templates/Gulpfile.js
@@ -4,22 +4,32 @@ var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
 
 gulp.task('serve', function() {
-  browserSync.init({
-      proxy: 'fdwl.dev/src/main/resources/templates'
-  });
+    browserSync.init({
+        proxy: 'fdwl.dev/src/main/resources/templates'
+    });
 
     gulp.watch('components/**/*.scss', ['styles']);
     gulp.watch('**/*.html').on('change', browserSync.reload);
     gulp.watch('*.html').on('change', browserSync.reload);
-    // gulp.watch('../public/*.js').on('change', browserSync.reload)
+    gulp.watch('../public/*.js', ['scripts']);
 });
 
 gulp.task('styles', function() {
     gulp.src('components/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('style.css'))
-        .pipe(gulp.dest('../public/css'))
+        .pipe(gulp.dest('../public'))
+        .pipe(browserSync.stream());
+
+    gulp.src('components/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('../../../../resources'))
         .pipe(browserSync.stream());
 });
+
+gulp.task('scripts', function(){
+  gulp.src('../public/*.js').pipe(gulp.dest('../../../../resources'));
+})
 
 gulp.task('default', ['serve']);
